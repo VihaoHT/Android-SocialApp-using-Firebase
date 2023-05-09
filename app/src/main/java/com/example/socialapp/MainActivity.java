cb9282a799811da4632da1464745da5c203a794b
VihaoHT <haoanna1232@gmail.com>
2023-05-09 19:33:47 +0700

    first commit

---
+++ app/src/main/java/com/example/socialapp/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.socialapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.socialapp.databinding.ActivityMainBinding;
+import com.example.socialapp.fragment.AddFragment;
+import com.example.socialapp.fragment.HomeFragment;
+import com.example.socialapp.fragment.NotificationFragment;
+import com.example.socialapp.fragment.ProfileFragment;
+import com.example.socialapp.fragment.SearchFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.iammert.library.readablebottombar.ReadableBottomBar;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    ProgressDialog dialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        dialog = new ProgressDialog(MainActivity.this);
+        dialog.setCancelable(false);
+        dialog.setMessage("Loading...");
+
+        setSupportActionBar(binding.toolbar);
+        MainActivity.this.setTitle("My Profile");
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        binding.toolbar.setVisibility(View.GONE);
+        transaction.replace(R.id.container, new HomeFragment());
+        transaction.commit();
+
+        binding.readableBottomBar.setOnItemSelectListener(new ReadableBottomBar.ItemSelectListener() {
+            @Override
+            public void onItemSelected(int i) {
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                switch (i){
+                    case 0:
+                        binding.toolbar.setVisibility(View.GONE);
+                        transaction.replace(R.id.container, new HomeFragment());
+                        break;
+                    case 1:
+                        binding.toolbar.setVisibility(View.GONE);
+                        transaction.replace(R.id.container, new NotificationFragment());
+                        break;
+                    case 2:
+                        binding.toolbar.setVisibility(View.GONE);
+                        transaction.replace(R.id.container, new AddFragment());
+                        break;
+                    case 3:
+                        binding.toolbar.setVisibility(View.GONE);
+                        transaction.replace(R.id.container, new SearchFragment());
+                        break;
+                    case 4:
+                        binding.toolbar.setVisibility(View.VISIBLE);
+                        transaction.replace(R.id.container, new ProfileFragment());
+                        break;
+                }
+                transaction.commit();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            //log out
+//            case R.id.setting:
+//                auth.signOut();
+//                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+//                startActivity(intent);
+//                finish();
+//                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
